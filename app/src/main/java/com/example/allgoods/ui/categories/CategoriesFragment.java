7a1d46d6b84ce28c8c3baeec9f8964adf69060a7
AllGoods <theinvinciblegroup23@gmail.com>
2020-12-04 03:48:43 +0800

    AllGoods

--- app/src/main/java/com/example/allgoods/ui/categories/CategoriesFragment.java
+++ app/src/main/java/com/example/allgoods/ui/categories/CategoriesFragment.java
@@ -1,19 +1,15 @@
 package com.example.allgoods.ui.categories;
 
-import android.app.FragmentTransaction;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.allgoods.R;
-import com.example.allgoods.Sidebar;
-import com.example.allgoods.StartingActivity;
 
 public class CategoriesFragment extends Fragment {
 
@@ -22,19 +18,8 @@ public class CategoriesFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.categories, container, false);
 
-
-        ImageView imageView = (ImageView) root.findViewById(R.id.emotional);
-        imageView.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 FragmentTransaction fragmentTransaction=getFragmentManager().beginTransaction();
-                 fragmentTransaction.replace(R.id.categories);
-             }
-         });
-
         return root;
 
     }
-
 
 }
